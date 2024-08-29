53db0a1f219e662d7ff292790aa45a5b4f412e7d
nmsaini <nmsaini@gmail.com>
2024-08-29 15:05:25 -0400

    Update install-ibm-pak-plugin.sh
    changed from podman to download directly

--- install-ibm-pak-plugin.sh
+++ install-ibm-pak-plugin.sh
@@ -8,16 +8,6 @@
 # 
 # skopeo list-tags docker://cp.icr.io/cpopen/cpfs/ibm-pak
 #
-PAK_VERSION=v1.15.2
-#
-# check podman or docker installed
-if ! ((command -v podman >/dev/null) || (command -v docker >/dev/null))
-then
-	echo "error: need podman or docker installed...."
-	exit 1
-fi
-
-# since we are installing ibm-pak we need to make sure we are root
 if [ "$EUID" -ne 0 ]
 then 
 	echo "Please run as root"
@@ -32,23 +22,8 @@ if [ $? -eq 0 ]; then
 	exit 1
 fi
 
-# 1 of them is installed check podman
-if (command -v podman >/dev/null)
-then
-	id=$(podman create cp.icr.io/cpopen/cpfs/ibm-pak:$PAK_VERSION - )
-	podman cp $id:/ibm-pak-plugin plugin-dir
-	podman rm -v $id
-	cd plugin-dir
-	tar -xvf oc-ibm_pak-linux-amd64.tar.gz
-	cp oc-ibm_pak-linux-amd64 /usr/local/bin/oc-ibm_pak
-else
-	id=$(docker create cp.icr.io/cpopen/cpfs/ibm-pak:$PAK_VERSION - )
-	docker cp $id:/ibm-pak-plugin plugin-dir
-	docker rm -v $id
-	cd plugin-dir
-	tar -xvf oc-ibm_pak-linux-amd64.tar.gz
-	cp oc-ibm_pak-linux-amd64 /usr/local/bin/oc-ibm_pak
-fi
+curl -sL https://github.com/IBM/ibm-pak/releases/latest/download/oc-ibm_pak-linux-amd64.tar.gz | tar zx
+mv oc-ibm_pak-linux-amd64 /usr/local/bin/oc-ibm_pak
 
-echo "ibm-pak installed...done"
-
+verInstalled=$(oc ibm-pak --version)
+echo "ibm-pak version $verInstalled installed...done"
