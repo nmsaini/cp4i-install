10274c3b920d07cbeeb0b859a89a61d24b8f2c20
nmsaini <nmsaini@gmail.com>
2024-08-29 14:56:08 -0400

    Update install-ibm-pak-plugin.sh

--- install-ibm-pak-plugin.sh
+++ install-ibm-pak-plugin.sh
@@ -3,6 +3,9 @@
 # more info https://github.com/IBM/ibm-pak#download-and-verify-software 
 # download directly instead of docker 
 # wget https://github.com/IBM/ibm-pak/releases/latest/download/oc-ibm_pak-linux-amd64.tar.gz
+# curl -sL https://github.com/IBM/ibm-pak/releases/latest/download/oc-ibm_pak-linux-amd64.tar.gz | tar zx && mv oc-ibm_pak-linux-amd64 /usr/local/bin/oc-ibm_pak && oc ibm-pak --version
+# mv oc-ibm_pak-linux-amd64 /usr/local/bin/oc-ibm_pak
+# 
 # skopeo list-tags docker://cp.icr.io/cpopen/cpfs/ibm-pak
 #
 PAK_VERSION=v1.15.2
